3359d06141efbc2968b090ab5fd96d1f65b83cda
Naksh Rathore <naksh.rathore786@gmail.com>
2025-05-07 10:24:43 -0500

    Update ListPrinter.java for 2 ways of printing
    Added static to the printList function to enable the usage of way 1 (not needing an instance) but still keep the the original way

--- generics/ListPrinter.java
+++ generics/ListPrinter.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 public class ListPrinter {
-  private <T> void printList(List<T> collection){
+  private static <T> void printList(List<T> collection){ // Remove static if you are doing way 2
     for(T item: collection){
       System.out.print(item + " ");
     }
@@ -14,15 +14,26 @@ public class ListPrinter {
     List<Double> doubles = List.of(1.1,2.2,3.3,4.4,5.5);
     List<Character> chars = List.of('J','O','S','D','E','M');
 
-    ListPrinter printer = new ListPrinter();
-    
+    // Way 1
     System.out.print("Integers: ");
-    printer.printList(integers);
+    printList(integers);
 
     System.out.print("Doubles: ");
-    printer.printList(doubles);
+    printList(doubles);
     
     System.out.print("Characters: ");
-    printer.printList(chars);
+    printList(chars);
+
+    // Way 2
+    // ListPrinter printer = new ListPrinter();
+    
+    // System.out.print("Integers: ");
+    // printer.printList(integers);
+
+    // System.out.print("Doubles: ");
+    // printer.printList(doubles);
+    
+    // System.out.print("Characters: ");
+    // printer.printList(chars);
   }
 }
